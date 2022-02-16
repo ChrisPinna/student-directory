61075f162bed8a89460c69bfc2573656cc9f4e7c
Christian Pinna <christian.a.pinna@gmail.com>
2022-02-16 09:36:44 +0000

    update to directory.rb: now the user can load a previouse student list from the students.csv file.   created .gitignore: students.csv created by the directory.rb program to store lists of students will only be stored locally and the .gitignore file stores the name of files to be ignored by Git

--- directory.rb
+++ directory.rb
@@ -38,6 +38,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -55,6 +56,8 @@ def proccess(selection)
       show_students
     when "3"
       save_file
+    when "4"
+      load_students
     when "9"
       exit # this will cause the program to terminate
     else
@@ -78,6 +81,17 @@ def save_file
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  }
+  file.close
+end
+
+def load_students
+  
+  file = File.open("students.csv", "r")
+  
+  file.readlines.each { |line| 
+    name, cohort = line.chomp.split(',')
+    @students << {:name => name, :cohort => cohort.to_sym}
   }
   file.close
 end
